78f804bb40c1122c93fc0d1b34280583268b4742
David Green <david.green@tasktop.com>
2015-09-07 15:26:13 -0700

    add tests for Handler

--- greensopinion.finance.services/src/main/java/greensopinion/finance/services/web/dispatch/Handler.java
+++ greensopinion.finance.services/src/main/java/greensopinion/finance/services/web/dispatch/Handler.java
@@ -2,7 +2,9 @@ package greensopinion.finance.services.web.dispatch;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedType;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.List;
@@ -13,11 +15,13 @@ import javax.ws.rs.PathParam;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
+import jersey.repackaged.com.google.common.base.Throwables;
+
 class Handler {
 	static final String NAME_ENTITY = "<request-entity>";
 
-	private Object webService;
-	private Method method;
+	private final Object webService;
+	private final Method method;
 
 	private List<String> parameterOrder;
 	private Map<String, Type> parameterNameToType;
@@ -25,7 +29,7 @@ class Handler {
 	public Handler(Object webService, Method method) {
 		this.webService = checkNotNull(webService);
 		this.method = checkNotNull(method);
-		compileParameters(method.getAnnotatedParameterTypes());
+		compileParameters(method.getAnnotatedParameterTypes(), method.getParameterAnnotations());
 	}
 
 	public Map<String, Type> getParameterNameToType() {
@@ -38,29 +42,43 @@ class Handler {
 			String parameterName = parameterOrder.get(x);
 			params[x] = parameters.get(parameterName);
 		}
-		return method.invoke(webService, params);
+		try {
+			return method.invoke(webService, params);
+		} catch (InvocationTargetException e) {
+			throw Throwables.propagate(e.getCause());
+		}
 	}
 
-	private void compileParameters(AnnotatedType[] annotatedParameterTypes) {
+	private void compileParameters(AnnotatedType[] annotatedParameterTypes, Annotation[][] annotations) {
 		ImmutableList.Builder<String> parameterOrderBuilder = ImmutableList.builder();
 		ImmutableMap.Builder<String, Type> nameToTypeBuilder = ImmutableMap.builder();
 
-		for (AnnotatedType annotatedParameter : annotatedParameterTypes) {
-			PathParam pathParam = annotatedParameter.getAnnotation(PathParam.class);
+		for (int x = 0; x < annotatedParameterTypes.length; ++x) {
+			AnnotatedType annotatedType = annotatedParameterTypes[x];
+			PathParam pathParam = getPathParam(annotations[x]);
 			String name;
 			Type type;
 			if (pathParam != null) {
 				name = pathParam.value();
-				type = annotatedParameter.getType();
+				type = annotatedType.getType();
 			} else {
 				name = NAME_ENTITY;
-				type = annotatedParameter.getType();
+				type = annotatedType.getType();
 			}
 			parameterOrderBuilder.add(name);
 			nameToTypeBuilder.put(name, type);
 		}
 		parameterOrder = parameterOrderBuilder.build();
 		parameterNameToType = nameToTypeBuilder.build();
+	}
+
+	private PathParam getPathParam(Annotation[] annotations) {
+		for (Annotation annotation : annotations) {
+			if (PathParam.class == annotation.annotationType()) {
+				return (PathParam) annotation;
+			}
+		}
+		return null;
 	}
 
 }
---
+++ greensopinion.finance.services/src/test/java/greensopinion/finance/services/web/dispatch/HandlerTest.java
@@ -0,0 +1,41 @@
+package greensopinion.finance.services.web.dispatch;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.PathParam;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.google.common.collect.ImmutableMap;
+
+public class HandlerTest {
+	public static class TestWebService {
+		public Object thrower() {
+			throw new RuntimeException("ouch");
+		}
+
+		public String echo(@PathParam("message") String s) {
+			return s;
+		}
+	}
+
+	@Rule
+	public final ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void invokeWithException() throws Exception {
+		Handler handler = new Handler(new TestWebService(), TestWebService.class.getDeclaredMethod("thrower"));
+		thrown.expect(RuntimeException.class);
+		thrown.expectMessage("ouch");
+		handler.invoke(ImmutableMap.of());
+	}
+
+	@Test
+	public void invokeWithParameter() throws Exception {
+		Handler handler = new Handler(new TestWebService(),
+				TestWebService.class.getDeclaredMethod("echo", String.class));
+		assertEquals("test it yeah!", handler.invoke(ImmutableMap.of("message", "test it yeah!")));
+	}
+}
