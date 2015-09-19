5fd6603991f362057d7dd5f21671ae3153ae56b0
David Green <david.green@tasktop.com>
2015-09-19 11:20:13 -0700

    add sorting to transactions listing

--- greensopinion.finance.ui/app/scripts/controllers/transactionsListing.js
+++ greensopinion.finance.ui/app/scripts/controllers/transactionsListing.js
@@ -12,9 +12,11 @@ angular.module('greensopinionfinanceApp')
 
     $scope.title = 'Transactions: '+ month.name;
 
+
     reportService.transactionsForMonth(month.id).then(function(periodTransactions) {
       $scope.periodTransactions = periodTransactions;
     });
+    
     $scope.incomeOf = function(transaction) {
       return transaction.amount > 0? money.format(transaction.amount) : '';
     };
@@ -26,5 +28,30 @@ angular.module('greensopinionfinanceApp')
     };
     $scope.ok = function() {
       $modalInstance.close();
+    };
+    $scope.sortExpense = function (transaction) {
+      var sortable = -transaction.amount;
+      console.log(''+sortable);
+      return sortable;
+    };
+    $scope.sortIncome = function (transaction) {
+      return transaction.amount;
+    };
+
+    $scope.sortType = $scope.dateOf;
+    $scope.sortReverse = false;
+
+    $scope.switchSortType = function(target) {
+      var previousSortType = $scope.sortType;
+      $scope.sortType = target;
+      if (previousSortType !== $scope.sortType) {
+        if ($scope.sortType === $scope.sortExpense || $scope.sortType === $scope.sortIncome) {
+          $scope.sortReverse = true;
+        } else {
+          $scope.sortReverse = false;
+        }
+      } else {
+        $scope.sortReverse = !$scope.sortReverse;
+      }
     };
   }]);
--- greensopinion.finance.ui/test/spec/controllers/transactionsListing.spec.js
+++ greensopinion.finance.ui/test/spec/controllers/transactionsListing.spec.js
@@ -17,6 +17,15 @@ describe('Controller: TransactionsListingCtrl', function () {
       transactions: [
         {
           date: '2015-02-28T23:41:00.023Z', description: 'a purchase', amount: -123456
+        },
+        {
+          date: '2015-02-30T23:41:00.023Z', description: 'a purchase2', amount: -140456
+        },
+        {
+          date: '2015-02-30T23:41:00.023Z', description: 'deposit1', amount: 10243
+        },
+        {
+          date: '2015-02-31T23:41:00.023Z', description: 'deposit2', amount: 2043
         }
       ]
     };
@@ -62,5 +71,32 @@ describe('Controller: TransactionsListingCtrl', function () {
   it('should expose periodTransactions in scope',function() {
     $rootScope.$digest();
     expect(scope.periodTransactions).toBe(periodTransactions);
+  });
+
+  it('should expose sorting',function() {
+    expect(scope.switchSortType).toBeDefined();
+    expect(scope.sortType).toBeDefined();
+    expect(scope.sortIncome).toBeDefined();
+    expect(scope.sortExpense).toBeDefined();
+
+    // default
+    expect(scope.sortType).toBe(scope.dateOf);
+    expect(scope.sortReverse).toBe(false);
+
+    scope.switchSortType(scope.dateOf);
+    expect(scope.sortType).toBe(scope.dateOf);
+    expect(scope.sortReverse).toBe(true);
+
+    scope.switchSortType(scope.dateOf);
+    expect(scope.sortType).toBe(scope.dateOf);
+    expect(scope.sortReverse).toBe(false);
+
+    scope.switchSortType(scope.sortIncome);
+    expect(scope.sortType).toBe(scope.sortIncome);
+    expect(scope.sortReverse).toBe(true);
+
+    scope.switchSortType(scope.sortExpense);
+    expect(scope.sortType).toBe(scope.sortExpense);
+    expect(scope.sortReverse).toBe(true);
   });
 });
